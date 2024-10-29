0efbde86f48b29ec46b15c32d5f69a59560b7da2
Liam Muro <liammuro123@icloud.com>
2024-10-29 12:11:13 -0400

    added java projects

---
+++ Java Projects/Prisoner Riddle/RiddleTests.java
@@ -0,0 +1,10 @@
+public class RiddleTests { 
+    public static void test1() {
+        for (int i = 1; i <= 50; i++) {
+
+        }
+    } 
+    public static void main(String[] args) {
+        test1();
+    }
+}
